66c0bbda1b6b34fb7f4aa5c963c1e9789c9230f0
moh-IA <mohamed.el.arbi.rabah@gmail.com>
2020-09-30 06:18:27 +0200

    add bonus 2

--- src/get_files.py
+++ src/get_files.py
@@ -1,19 +1,27 @@
 import requests
 import zipfile
+import os 
 
-url = 'https://stdatalake005.blob.core.windows.net/public/movies_dataset.zip'
+
 
 
 def download_file (url):
+    if not os.path.exists('../../data.zip'):
+        r = requests.get(url)
+        with open('../../data.zip','wb') as f:
+            f.write(r.content)
+    else:
+        print("data.zip existe déja")
+
+    if not os.path.exists('../../data'):
+         with zipfile.ZipFile('../../data.zip', 'r') as data_zip:
+            data_zip.extractall('../../data')
+    else:
+        print("data existe déja")
     
-    r = requests.get(url)
-    with open('data.zip','wb') as f:
-        f.write(r.content)
 
-    with zipfile.ZipFile('data.zip', 'r') as data_zip:
-        data_zip.extractall('data')
 
-download_file(url)
+
 
 
 
